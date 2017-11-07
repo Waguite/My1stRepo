7587af207bf2e3bb087f383201945049db50e4e0
Waguite <33459317+Waguite@users.noreply.github.com>
2017-11-07 10:47:09 -0500

    innitial commit

---
+++ personality_WG.py
@@ -0,0 +1,35 @@
+name= "Ware"
+state= "connecticut"
+city= "New York"
+book= "of Mice and Men"
+sport= "archery"
+print(name + "likes to visit" + state + "and read" + book + "in" + city)
+food= "chicken"
+hobby= "cubing"
+instrument= "trumpet"
+musicpiece= "so what"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("whats your favorite subject?")
+subject= input
+if subject == "computer science":
+    print("me too!")
+else:
+    print(subject + " is pretty good too!")
---
+++ personality_WG_survey.py
@@ -0,0 +1,26 @@
+print ("Whats your favorite color?")
+color =  input()
+if color== "blue":
+    print("orange is better.")
+elif color == "red":
+    print ("mine too!")
+print ("what's your favorite subject?")
+subject = input()
+if subject == "Mandarin" or subject == "English":
+    print("thats one of my favorites too!")
+else:
+    print (input + "is a great course.")
+
+print("whats your favorite sport?")
+sport = input()
+if sport == "archery":
+    print ("thats my favorite sport too!")
+elif sport == "soccer":
+    print ("cool! what position do you play?")
+if input == "forward":
+    print ("cool! I used to play that position!")
+
+print ("what's your favorite animal?")
+animal = input ()
+if animal == "dog" or animal== "cat":
+    print ("I love" + input +"s!" )
